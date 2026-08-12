2f31b360f2dbc51e85a0a96bf254411f90802ce2
Chenjacky32 <Chenjacky32@gmail.com>
2026-08-12 12:57:03 +0700

    feat: implement Get user detail.
    - Add routing for user detail endpoint
    - Create GetUserDetailController
    - Implement GetUserDetail Action classes
    - Integrate routing, request validation, payload/DTO transformation, business logic execution, and response handling
    
    Resolves #9

---
+++ app/Actions/Users/GetUserDetail.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Users;
+
+use App\Models\User;
+
+
+final readonly class GetUserDetail
+{
+    public function handle(string $id):User
+    {
+        return User::findOrFail($id);
+    }
+}
---
+++ app/Http/Controllers/Users/V1/GetUserDetailController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Users\V1;
+
+use App\Actions\Users\GetUserDetail;
+use App\Http\Responses\JsonDataResponse;
+
+final readonly class GetUserDetailController
+{
+    public function __construct(
+        private GetUserDetail $getUserDetail
+    )
+    {
+    }
+
+    public function __invoke(string $id): JsonDataResponse
+    {
+        $userDetails = $this->getUserDetail->handle($id);
+
+        return new JsonDataResponse(
+            data:[
+                'id' => $userDetails->id,
+                'name' => $userDetails->name,
+                'email' => $userDetails->email,
+                'role' => $userDetails->role?->role,
+                'isActive' => $userDetails->is_active,
+                'createdAt'=> $userDetails->created_at->toDateTimeString(),
+                'updatedAt'=> $userDetails->updated_at->toDateTimeString()
+            ],
+            message: 'Fetch users detail successfully',
+        );
+    }
+}
